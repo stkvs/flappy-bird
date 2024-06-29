3ba1f4c167f8fbdef3e64c93a1a8c4c5f8e0d792
veryrarestat <stmantaz@gmail.com>
2024-06-29 04:22:06 +0100

    Added files and created baseplate

---
+++ script/index.js
@@ -0,0 +1,9 @@
+const gameArea = document.querySelector('.gameArea');
+
+const background = document.createElement('div');
+background.classList.add('background');
+gameArea.appendChild(background);
+
+const bird = document.createElement('div');
+bird.classList.add('bird');
+gameArea.appendChild(bird);
